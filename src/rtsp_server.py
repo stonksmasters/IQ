eedf727f6f66ddef311aa77414f64f586504caa9
stonksmasters <john.borg237@gmail.com>
2025-01-09 14:47:21 -0600

    gi

--- src/rtsp_server.py
+++ src/rtsp_server.py
@@ -1,4 +1,6 @@
 import gi
+print("PyGObject installed successfully!")
+
 gi.require_version('Gst', '1.0')
 gi.require_version('GstRtspServer', '1.0')
 from gi.repository import Gst, GstRtspServer, GObject
